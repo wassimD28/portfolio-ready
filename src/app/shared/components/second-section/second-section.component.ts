9e9e06301cc1b84c4bbf9d6b4dc9633aaa4dfa66
wassim <wassim28migalolosaad@gmail.com>
2024-06-18 16:48:56 +0100

    Rearranging the projects

--- src/app/shared/components/second-section/second-section.component.ts
+++ src/app/shared/components/second-section/second-section.component.ts
@@ -12,16 +12,6 @@ export class SecondSectionComponent implements AfterViewInit{
 
   projects : Project []= [
     {
-      title: 'Pomodoro timer',
-      description: 'At the beginning of 2022, I created a Pomodoro timer web app using HTML, CSS, and JavaScript. This project was designed to help users manage their time more effectively by following the Pomodoro Technique, which breaks work into intervals, typically 25 minutes in length, separated by short breaks. Through this project, I enhanced my front-end development skills and learned how to implement dynamic functionalities with JavaScript.',
-      image: 'pomodoro-app.jpg',
-      usedTechnology : [
-        {name: 'HTML', image: 'html.png'},
-        {name: 'CSS', image: 'css.png'},
-        {name: 'JavaScript', image: 'js.png'}
-      ]
-    },
-    {
       title : 'Training website',
       description:'At the ending of 2023, I developed a training website web app using Angular for the front end and pure PHP for the backend. This project aimed to provide an interactive platform for users to access various training modules and track their progress. The Angular framework allowed me to create a dynamic and responsive user interface, while the PHP backend handled user authentication, data storage, and retrieval. Through this project, I gained valuable experience in full-stack development, particularly in integrating a modern front-end framework with a traditional server-side language.',
       image: 'training-app.jpeg',
@@ -37,7 +27,17 @@ export class SecondSectionComponent implements AfterViewInit{
         {name: 'symfony', image: 'symfony-white.png'},
         {name: 'tailwind', image: 'Tailwind CSS.png'},
       ]
-    }
+    },
+    {
+      title: 'Pomodoro timer',
+      description: 'At the beginning of 2022, I created a Pomodoro timer web app using HTML, CSS, and JavaScript. This project was designed to help users manage their time more effectively by following the Pomodoro Technique, which breaks work into intervals, typically 25 minutes in length, separated by short breaks. Through this project, I enhanced my front-end development skills and learned how to implement dynamic functionalities with JavaScript.',
+      image: 'pomodoro-app.jpg',
+      usedTechnology : [
+        {name: 'HTML', image: 'html.png'},
+        {name: 'CSS', image: 'css.png'},
+        {name: 'JavaScript', image: 'js.png'}
+      ]
+    },
   ]
 
   @Input() tl!: GSAPTimeline;
